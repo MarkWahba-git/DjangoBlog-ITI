031595f43d001270e7fa190e56524ebc4bb4d560
nouran <nouranebrahim9@gmail.com>
2020-02-23 23:26:30 +0200

    add models to admin.py

--- DjangoBlogITI/djangoBlog/admin.py
+++ DjangoBlogITI/djangoBlog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from djangoBlog.models import forbidden_words,category,subscribe
 
 # Register your models here.
+admin.site.register(forbidden_words)
+admin.site.register(category)
+admin.site.register(subscribe)
