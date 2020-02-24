bf82eff0f854dced2926c99597f66c96c836d3bd
nouran <nouranebrahim9@gmail.com>
2020-02-24 11:02:57 +0200

    forbidden_words model

--- DjangoBlogITI/adminBlog/models.py
+++ DjangoBlogITI/adminBlog/models.py
@@ -6,3 +6,8 @@ class category(models.Model):
 	name=models.CharField(max_length=50)
 	def __str__(self):
 		return self.name
+class forbidden_words(models.Model):
+	name=models.CharField(max_length=40)
+	def __str__(self):
+		return self.name
+
